ecd28914d135b11ca4a32bdba286c1fd0b3ac4f8
Vicente Torres <129629302+vicentetorresr@users.noreply.github.com>
2023-07-18 18:10:20 -0400

    Delete ejercicios_basicos directory

--- ejercicios_basicos/juegoAdivinaElNumero.py
+++
@@ -1,9 +0,0 @@
-import random
-numR = random.randint(1,5)
-while True:
-  num=int(input("Adivina el número: "))
-  if num==numR:
-    print("Adivinaste")
-    break
-  else:
-    print("Incorrecto")
--- ejercicios_basicos/palindromo.py
+++
@@ -1,7 +0,0 @@
-txt = input("Ingrese un texto: ")
-reversed_txt = ''.join(reversed(txt))
-
-if txt == reversed_txt:
-    print("La frase es un palíndromo")
-else:
-    print("La frase no es un palíndromo")
